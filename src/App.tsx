7b521155c9c93d560e64aa509b0605022162483d
Kirill <kirillpuzanov@mail.ru>
2020-06-25 00:05:47 +0300

    Home_Work_1

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import {Message} from "./Components/Message";
+
+function App() {
+
+    return (
+        <div className="App">
+            <Message src="https://avatars.mds.yandex.net/get-pdb/2301590/a92c498e-7910-4b77-b246-2afa0e5943fd/s375" name="Ivan" text=" Hello World! Hello World! Hello World! Hello World!  "/>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/Components/Message.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import st from './Message.module.css';
+
+type MessagePropsType = {
+    src: string,
+    name: string,
+    text: string | number | symbol
+
+}
+
+
+export function Message(props: MessagePropsType) {
+    return (
+        <div className={st.message_wrapper}>
+            <div className={st.message_inner}>
+                <div><img className={st.message_ava} src={props.src} alt="#"/></div>
+                <div className={st.message_cont}>
+                    <span className={st.message_name}>{props.name} </span>
+                    <div className={st.message_text}>{props.text}</div>
+                    <span className={st.message_date}> {new Date().getHours() + '.' + new Date().getMinutes()}</span>
+                </div>
+            </div>
+        </div>
+    )
+}
