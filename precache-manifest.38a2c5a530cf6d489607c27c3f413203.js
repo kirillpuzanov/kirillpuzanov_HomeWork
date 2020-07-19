6c142d9b020c523782099661907753f819a3b07f
kirillpuzanov <kirillpuzanov@mail.ru>
2020-07-19 19:20:14 +0300

    Updates

---
+++ precache-manifest.38a2c5a530cf6d489607c27c3f413203.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b864ccac0f317cb597bc472131b528f",
+    "url": "/kirillpuzanov_HomeWork/index.html"
+  },
+  {
+    "revision": "1c00242d253ed25dea21",
+    "url": "/kirillpuzanov_HomeWork/static/css/main.fca0373c.chunk.css"
+  },
+  {
+    "revision": "d8bbabe0662a41679de6",
+    "url": "/kirillpuzanov_HomeWork/static/js/2.04551ae0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kirillpuzanov_HomeWork/static/js/2.04551ae0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c00242d253ed25dea21",
+    "url": "/kirillpuzanov_HomeWork/static/js/main.ee57e8a1.chunk.js"
+  },
+  {
+    "revision": "cb633d4d754e2cc22ad9",
+    "url": "/kirillpuzanov_HomeWork/static/js/runtime-main.2fb146c3.js"
+  }
+]);
